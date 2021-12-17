b2e1dfc087f5196a91c21836924795bb4e9f8740
KrabikKostya <kostya.ktabik.sw@gmail.com>
2021-12-17 21:01:11 +0200

    18 commit

--- project/urls.py
+++ project/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("TaskManager/", include("taskmanager.urls")),
     path('admin/', admin.site.urls),
+    path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/x-javascript'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
