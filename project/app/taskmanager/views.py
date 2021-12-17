5a69d1691dff989d99ce1de3488ef9f296a3a09e
KrabikKostya <kostya.ktabik.sw@gmail.com>
2021-12-17 16:24:14 +0200

    9 commit

--- project/app/taskmanager/views.py
+++ project/app/taskmanager/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from .forms import TasckForm
 from .models import Tasck
@@ -19,6 +20,7 @@ def form(request):
             form.save()
             return redirect('index')
         else:
+            form.add_error(None, "Ваша задача накладывается на другую задачу")
             error = "Ошибка валидации, проверьте, правильно ли вы заполнили все поля, скорее всего ваша задача накладывается на другую задачу"
     form = TasckForm
     data ={
