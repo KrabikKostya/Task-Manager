63a36bd4a66c46ed5fa2dce9be61587fc25059ae
KrabikKostya <kostya.ktabik.sw@gmail.com>
2022-01-06 19:43:53 +0200

    54 commit

--- project/app/taskmanager/urls.py
+++ project/app/taskmanager/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 
@@ -7,5 +8,5 @@ urlpatterns = [
     path("form/form.html", views.form, name="form"),
     path("<int:id>/", views.edit, name="edit"),
     path("form/<int:id>/form_edit.html", views.edit_task, name="edit_task"),
-    path("calendar/calendar.html", views.calendar, name="calendar"),
+    path("calendar/calendar.html", views.CalendarView.as_view(), name='calendar'),
 ]
--- project/app/taskmanager/views.py
+++ project/app/taskmanager/views.py
@@ -1,3 +1,10 @@
+from datetime import datetime
+from .utils import Calendar
+from .models import *
+from django.utils.safestring import mark_safe
+from django.views import generic
+from django.http import HttpResponse
+from django.shortcuts import render
 from datetime import date, timedelta, time
 from django.shortcuts import render, redirect
 from .forms import TasckForm
@@ -100,9 +107,35 @@ def edit_task(request, id):
     return render(request, "taskmanager/form_edit.html", data)
 
 
-def calendar(request):
-    data = {
-        "form": form,
-        "tascks": Tasck.objects.all()
-    }
-    return render(request, 'taskmanager/calendar.html', data)
+# def calendar(request):
+#     data = {
+#         "form": form,
+#         "tascks": Tasck.objects.all()
+#     }
+#     return render(request, 'taskmanager/calendar.html', data)
+
+
+class CalendarView(generic.ListView):
+    model = Tasck
+    # template_name = 'cal/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('day', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
